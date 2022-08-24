bc5e1d435301fd4af71bbb6b6b182d9326ec99e0
Golanukonda Sai kumar <61460608+tronkiller1919@users.noreply.github.com>
2022-08-24 20:29:47 +0530

    Sun

--- Angular-Frontend/src/app/service/apiServices/api-uri.service.ts
+++ Angular-Frontend/src/app/service/apiServices/api-uri.service.ts
@@ -7,14 +7,14 @@ export class ApiUriService {
   constructor() {}
 
   readonly SIGNUP_API_URI: string =
-    "http://localhost:8992/auth/signup";
+    "http://localhost:9003/auth/signup";
 
   readonly LOGIN_API_URI: string =
-    "http://localhost:8992/auth/login";
+    "http://localhost:9003/auth/login";
 
   readonly PROCESS_COMPONENT_API_URI: string =
-    "http://localhost:8993/component-process/process-request";
+    "http://localhost:9004/component-process/process-request";
 
   readonly CONFIRM_ORDER_API_URI: string =
-    "http://localhost:8993/component-process/complete-processing";
+    "http://localhost:9004/component-process/complete-processing";
 }
